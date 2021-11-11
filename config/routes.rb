96fe3b25e9598ba6d093ffd948496b64ed36f657
Damian Wdowiak <damian.wdowiak@htdevelopers.com>
2021-11-11 16:57:06 +0100

    reformat routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     namespace :v1, defaults: { format: :json } do
       resources :firsts, only: %i[index]
       resources :workspaces do
-        resources :boards, only: %i[index show create update destroy]
+        resources :boards
       end
     end
   end
